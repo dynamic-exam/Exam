e7d3d812ed90e4359f5970f6d80c8c08370c2657
dynamic-exam <tamim06.ctg.bd@gmail.com>
2026-06-17 13:42:31 +0600

    Add files via upload

---
+++ Database/q3.js
@@ -0,0 +1,83 @@
+var title = '48th Special BCS Questions';
+var time = 60;
+var mark = 1;
+var negMark = 0.5;
+var opLabel = ['A', 'B', 'C', 'D'];
+var sheet = 'https://script.google.com/macros/s/AKfycbzbe0lg7LfiEfvniXHnnjLeKKD-050RfWOrFG2gyfj2PdYuWpdeEpNh0Bu4VWc_iGSA/exec';
+var questions = [
+    {
+        question: 'যিদ U = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, P = {1, 2, 5} এবং Q = {6, 7} হয় তেব P ∩ Q‘ এর মান কত? ',
+        options:['P','Q','Q‘','P‘'],
+        corrAns:0
+    },
+    {
+        question: ' ৮৮. ২ ঘন্টা ৪০ মিনট ৬ ঘন্টার কত অংশ?',
+        options:[' ১/৬','১/৫','৪/৯','১/৪'],
+        corrAns:2
+    },
+    {
+        question: '<img src="qAssets/1.png" width="100%">',
+        options:[' 3/2',' 2/3', ' 5/2','2/5'],
+        corrAns:0
+    },
+    {
+        question: ' যদি log10x = - 3 হয়, তেব x এর মান কত?',
+        options:['0.1','0.01','0.001','0.0001'],
+        corrAns:2
+    },
+    {
+        question: 'Symptoms of mild shock is',
+        options:['Rise of respiratory rate','Fall of blood pressure','Reduced Urinary output','Coma'],
+        corrAns:0
+    },
+    {
+        question: '<img src="qAssets/2.png" width="100%">',
+        options:['1','0','2','1/2'],
+        corrAns:2
+    },
+    {
+        question: 'Majority of body fluid remains in',
+        options:['Blood','Intra cellular fluid','Interstitial fluid','Lymph'],
+        corrAns:1
+    },
+    {
+        question: 'Complication of Caesarean section is',
+        options:['Postpartum Hemorrhage','Vaginal stenosis', 'Low platelet count','Injury to liver'],
+        corrAns:0
+    },
+    {
+        question: 'The antigen presenting cell on skin is',
+        options:['Basal cells','Keralinocytes','Langerhan cells','Melanocytes'],
+        corrAns:2
+    },
+    {
+        question: 'The largerst tributaries that drain into inferior Vena Cava is',
+        options:['Left renal Vein','Lumber Veins','Hepatic Veins','Right Renal Vein'],
+        corrAns:2
+    },
+    {
+        question: 'Carpal tunnel consists of',
+        options:['Median nerve','Musculocutaneous nerve','Radial nerve','Ulnar nerve'],
+        corrAns:0
+    },
+    {
+        question: 'Late sign of death is',
+        options:['adipocere formation', 'cooling of body','dilatation of pupils','rigor mortis'],
+        corrAns:0
+    },
+    {
+        question: 'Genital fistula can be prevented by',
+        options:['Improving essential obstetric care','Early operations of fibroid uterus',' Treating PID cases', 'correcting maternal anaemia'],
+        corrAns:0
+    },
+    {
+        question: 'Indication of hospitalization of a burn patient is',
+        options:['5% burn in the trunk of an adult patient','10% burn in the trunk of a child','Inhalation burn','10% Circumferential burn in the lower limb'],
+        corrAns:2
+    },
+    {
+        question: 'Which of the following is not a typical presentation of pain in acute pancreatitis?',
+        options:['Severe constant upper abdominal pain','Radiation pain to back','Absent muscle guarding','Presence of rebound tenderness'],
+        corrAns:2
+    }
+];
