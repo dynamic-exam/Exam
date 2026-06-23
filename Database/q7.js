0b67ea3018ee1a7a41966fa609eb07c535cbe8c7
dynamic-exam <tamim06.ctg.bd@gmail.com>
2026-06-23 21:56:53 +0600

    Add files via upload

---
+++ Database/q7.js
@@ -0,0 +1,508 @@
+var title = '39th Special BCS | Medical (51 - 100)';
+var time = 35;
+var mark = 1;
+var negMark = 0.5;
+var opLabel = ['A', 'B', 'C', 'D'];
+var sheet = 'https://script.google.com/macros/s/AKfycbxFRSAiJoSIEFqcSGetUJNDhTj6eiKfbxcHd1gTzDNVbeDARAoOf0woHLpUQ5y9CNNU/exec';
+var questions = [
+    {
+        question: 'Drug causing increased uric acid level is:',
+        options: [
+            'Pyrazinamide',
+            'Steroid',
+            'Colchichine',
+            'Levofloxacin'
+        ],
+        corrAns: 0
+},
+{
+        question: 'Immediate blood transfusion reaction includes:',
+        options: [
+            'Air embolism',
+            'Hypercalcemia',
+            'Hypokalacmia',
+            'Circulating overload'
+        ],
+        corrAns: 0
+},
+{
+        question: 'Which one of the following acts as anti oxidant?',
+        options: [
+            'Ceroloplasmin',
+            'Jonizing radiation',
+            'Hydrozen per oxide',
+            'Hypochlorous acid'
+        ],
+        corrAns: 0
+},
+{
+        question: 'The tissue macrophage is derived from:',
+        options: [
+            'Lymphocyte',
+            'Monocyte',
+            'Eosinophil',
+            'Helper T cell'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Pre XDR tuberculosis means:',
+        options: [
+            'TB resistant to isoniazid and rifampicin',
+            'TB resistant to isoniazid and ciprofloxacin',
+            'TB resistant to isoniazid, rifampicin and ciprofloxacin',
+            'TB resistant to isoniazid, rifampicin and pyrazinamide'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Serum sodium level is:',
+        options: [
+            '130-140 mmol/L',
+            '125-140 mmol/L',
+            '135-145 mmol/L',
+            '145-155 mmol/L'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Which vitamin is not synthesised in body?',
+        options: [
+            'Vit-D',
+            'Vit-K',
+            'Vit-A',
+            'Biotin'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Extra pyramidal lesion causes:',
+        options: [
+            'Flacid paralysis',
+            'Hyper kinesia',
+            'Cog wheel rigidity',
+            'Hyper reflexion of lower limb'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Which component is not present in CSF?',
+        options: [
+            'Glucose',
+            'Protein',
+            'Erythrocyte',
+            'Leucocyte'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Medial surface of brain is supplied by:',
+        options: [
+            'Middle cerebral arlery',
+            'Anterior cerebral artery',
+            'Verlebral artery',
+            'Porterior inferior cerebellar artery'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Urinary bladder is lined by.',
+        options: [
+            'Squamous epithelium',
+            'Transitional epithelium',
+            'Columner epithelium',
+            'Cuboidal epithelium'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Exotoxin mediated disease is:',
+        options: [
+            'Necrotizing fascitis',
+            'Wegener’s gramulomatosis',
+            'Kuru',
+            'Nipah encephalitis'
+        ],
+        corrAns: 0
+},
+{
+        question: 'Which of the following malagnancies is associated with radiation?',
+        options: [
+            'Ovarian carcinoma',
+            'Maltoma',
+            'Burkitt’s lymphoma',
+            'Acute lyumphoblastic leukaemia'
+        ],
+        corrAns: 3
+},
+{
+        question: 'Cardinal feature of atopic dermatitis is:',
+        options: [
+            'Swelling',
+            'Scratching',
+            'Widespread oily skin',
+            'Increased IgM level'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Morphological appearance of necrosis is the result of:',
+        options: [
+            'Denaturation of intracellular proteins',
+            'Saponification of cells',
+            'Mummification of cells',
+            'Intra celluler alkalosis'
+        ],
+        corrAns: 0
+},
+{
+        question: 'Elastic cartilage is not found in:',
+        options: [
+            'Auditory tube',
+            'Epiglottis',
+            'Auricle of ear',
+            'Epiphyseal plate of bone'
+        ],
+        corrAns: 3
+},
+{
+        question: 'Simple squamous epithelium is found in:',
+        options: [
+            'Lining of gall bladder',
+            'Lining of urinary bladder',
+            'Lining of fallopian tube',
+            'Endothelium'
+        ],
+        corrAns: 3
+},
+{
+        question: 'BCG vaccine at birth causes:',
+        options: [
+            'Prevention of primary tuberculosis',
+            'Prevention of secondary tuberculosis',
+            'Controlled primary infection',
+            'Prevention of MDR tuberculosis'
+        ],
+        corrAns: 2
+},
+{
+        question: 'During accommodation reaction there is:',
+        options: [
+            'Constriction of pupil by sympathetic nerve',
+            'Contraction of suspensory ligament',
+            'Increase in transeverse diameter of len',
+            'Contraction of ciliary musele'
+        ],
+        corrAns: 3
+},
+{
+        question: 'The drug of choice for cerebral malaria is:',
+        options: [
+            'Oral quinine',
+            'Injectable artesunat',
+            'Oral doxycycline',
+            'Combination of sulphametoxazole and pyremethamine'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Karyotype of Turner Syndrom is:',
+        options: [
+            '45 XO',
+            '45 XY',
+            '46 XX',
+            '47 XXY'
+        ],
+        corrAns: 0
+},
+{
+        question: 'Male external genital organ includes:',
+        options: [
+            'Seminal vesicle',
+            'Epididymis',
+            'Penis',
+            'Prostate'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Extracellular structural protein is:',
+        options: [
+            'Elastin',
+            'Keratin',
+            'Tubulin',
+            'Gastrin'
+        ],
+        corrAns: 0
+},
+{
+        question: 'Features of fibroadenoma include:',
+        options: [
+            'Usually multiple',
+            'Recetangular in shape',
+            'Common age after 30 years',
+            'Stroma is scanty'
+        ],
+        corrAns: 0
+},
+{
+        question: 'DNA is found in:',
+        options: [
+            'Golgi complex',
+            'Ribosome',
+            'Chromosome',
+            'Lysosome'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Confirmatory marker for Virimic stage of Hepatitis B virus (HBV) infection is:',
+        options: [
+            'HBsAg',
+            'Anti-HBs',
+            'HBV DNA',
+            'Anti-HBcgG'
+        ],
+        corrAns: 2
+},
+{
+        question: 'The rectum:',
+        options: [
+            'Begins infront of the 1st sacral vertebra',
+            'Has a lining of stratified squamous epithelium',
+            'Is relaled anteriorly to the cervix',
+            'Has a venous drainage into the superior vena cava'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Milk ejection in female is caused by:',
+        options: [
+            'Oestrogen',
+            'Progesterone',
+            'Prolactin',
+            'Oxytocin'
+        ],
+        corrAns: 3
+},
+{
+        question: 'Mutable risk factor of hypertension is:',
+        options: [
+            'Age',
+            'Smoking',
+            'Sex',
+            'Family history'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Which of the following is a tumoros marker?',
+        options: [
+            'PSA',
+            'CA 19-9',
+            'CRP',
+            'Family history'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Christmas disease develops due to deficiency of clotting factor:',
+        options: [
+            'V',
+            'VII',
+            'VIII',
+            'IX'
+        ],
+        corrAns: 3
+},
+{
+        question: 'Acute heart failure may occur in:',
+        options: [
+            'Rheumatoid arthritis',
+            'Rheumatic fever',
+            'Myxedema',
+            'Laparomatous condition'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Detrusor muscle is a muscle of:',
+        options: [
+            'Scrotum',
+            'Spermatic cord',
+            'Urogenital diaphragm',
+            'Urinary bladder'
+        ],
+        corrAns: 3
+},
+{
+        question: 'Amount of Hb contained in late normoblast is:',
+        options: [
+            '20 Pg',
+            '25 Pg',
+            '30 Pg',
+            '35 Pg'
+        ],
+        corrAns: 2
+},
+{
+        question: 'X-linked recessive disorder is:',
+        options: [
+            'Alkaptonuria',
+            'Hemophilia',
+            'Polycystic kidney disease',
+            'Wilson\'s disease'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Parasitic infection associated with anemia is:',
+        options: [
+            'Loa loa',
+            'W. bancrofti',
+            'Onchocerca volvulus',
+            'Necator americanus'
+        ],
+        corrAns: 3
+},
+{
+        question: 'Which of the following structure passes through the aortic opening of the diaphragm?',
+        options: [
+            'Azygos vein',
+            'Inferior venacava',
+            'Thoracic duct',
+            'Vagal trunk'
+        ],
+        corrAns: 2
+},
+{
+        question: 'In infants, defecation often follows a meal. The cause of colonic contractions in the situation is:',
+        options: [
+            'Histamine',
+            'Increased circulating level of CCK',
+            'The gastrocolic reflex',
+            'The enterogastric reflex'
+        ],
+        corrAns: 2
+},
+{
+        question: 'The supporting cells of nervous system is called:',
+        options: [
+            'Neuron',
+            'Neuroglia',
+            'Ganglia',
+            'Purkinjec cell'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Which disease is the mimicking anxiety disorder?',
+        options: [
+            'Alcohol withdrawal',
+            'Hypothyroidism',
+            'Hypoglycaemia',
+            'Temporal lobe epilepsy'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Medial boundary of the femoral ring is formed by:',
+        options: [
+            'Pectineus muscle',
+            'Inguinal ligament',
+            'Lecunar ligament',
+            'Femoral vein'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Following are the causes of vertigo except:',
+        options: [
+            'Benign positional vertigo',
+            'Acoustic neuroma',
+            'Vestibular neuritis',
+            'Bell\'s palsy'
+        ],
+        corrAns: 3
+},
+{
+        question: 'Non cardiac causes of atrial fibrillation is:',
+        options: [
+            'Pneumonia',
+            'Chronic liver disease',
+            'Chronic kidney disease',
+            'Stroke'
+        ],
+        corrAns: 0
+},
+{
+        question: 'Compared to hepatic bile, gall bladder bile contains a reduced concentration of which of the following?',
+        options: [
+            'Water',
+            'Bile acids',
+            'Protein',
+            'Glucose'
+        ],
+        corrAns: 0
+},
+{
+        question: 'All are functions of epithelial tissue except:',
+        options: [
+            'Absorption',
+            'Secretion',
+            'Contractility',
+            'Globulin synthesis'
+        ],
+        corrAns: 3
+},
+{
+        question: 'The major patho physiologic abnormality of Dengue Shock Syndrome is:',
+        options: [
+            'High haematocrit',
+            'Leukopenia',
+            'Plasma leakage',
+            'Thrombocytopenia'
+        ],
+        corrAns: 2
+},
+{
+        question: 'Drug used in "Scabies" is:',
+        options: [
+            'Permethrin',
+            'Streptomycin',
+            'Sulfasalazine',
+            'Ivermectin'
+        ],
+        corrAns: 0
+},
+{
+        question: 'During sleep, there is fall in the circulating level of hormone except:',
+        options: [
+            'Adrenaline',
+            'Cortisol',
+            'Growth hormone',
+            'Insulin'
+        ],
+        corrAns: 2
+},
+{
+        question: 'In adult, the daily loss of fluid in the feces, normally is:',
+        options: [
+            '50 ml',
+            '100 ml',
+            '150 ml',
+            '200 ml'
+        ],
+        corrAns: 1
+},
+{
+        question: 'Plasma clearance value of glucose is:',
+        options: [
+            '0 ml/min',
+            '75 ml/min',
+            '125 ml/min',
+            '140 ml/min'
+        ],
+        corrAns: 0
+}
+]
